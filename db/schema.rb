24acc29bed2c305f2d814b94c7ff2e7187ef7d46
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-08-07 17:31:33 -0400

    Add dot-env and swapped sqlite out for postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160807211848) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "climates", force: :cascade do |t|
     t.integer  "temperature"
     t.integer  "humidity"
